47944346e09bc50dc36a4053617d6657455d5d5f
petefox.igloo@gmail.com <petefox.igloo@gmail.com>
2025-07-06 11:47:41 +0000

    Created BalanceBar, BalanceBar.RowTemplate1, HomePage, OddsTable and 2 more; Edited settings; Deleted Form1

---
+++ client_code/BalanceBar/__init__.py
@@ -0,0 +1,12 @@
+from ._anvil_designer import BalanceBarTemplate
+from anvil import *
+
+class BalanceBar(BalanceBarTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    # Any code you write here will run before the form opens.
+
+  def update_info(self, balance, race_number, total_races):
+    self.lbl_balance.text = f"Balance (£): {balance}"
+    self.lbl_races.text = f"Race Number: {race_number}/{total_races}"
--- client_code/HomePage/__init__.py
+++ client_code/HomePage/__init__.py
@@ -1,9 +1,10 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import HomePageTemplate
 from anvil import *
 
-class Form1(Form1Template):
+class HomePage(HomePageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.balance_bar_1.update_info(balance=200.0, race_number=3, total_races=7)
