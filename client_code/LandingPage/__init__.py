f36753e16ceebdc01c7b800c0466586b46d58cf1
petefox.igloo@gmail.com <petefox.igloo@gmail.com>
2025-07-14 13:50:28 +0000

    Edited settings and LandingPage

--- client_code/LandingPage/__init__.py
+++ client_code/LandingPage/__init__.py
@@ -1,7 +1,6 @@
 from ._anvil_designer import LandingPageTemplate
 from anvil import *
 import anvil.server
-import anvil.storage
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -12,9 +11,9 @@ class LandingPage(LandingPageTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    if 'participant_id' not in anvil.storage.local:
+    if 'participant_id' not in anvil.users.SessionStorage:
       new_id = str(uuid.uuid4())
-      anvil.storage.local['participant_id'] = new_id
+      anvil.users.SessionStorage["participant_id"] = new_id
       anvil.server.call('store_participant_id',new_id)
 
   def start_button_click(self, **event_args):
